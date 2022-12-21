269b7fe6cf2d24742008ec1b1a0223e06a439340
Anthony Giannell <anthony-giannell@pluralsight.com>
2022-12-21 16:42:02 -0700

    completed module 2

--- src/app.js
+++ src/app.js
@@ -10,9 +10,35 @@ app.use(express.static(path.join(__dirname, '/public/')))
 app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'ejs')
 
+const accountData = fs.readFileSync(path.join(__dirname, './json/accounts.json'), { encoding: 'UTF8' })
+const userData = fs.readFileSync(path.join(__dirname, './json/users.json'), { encoding: 'UTF8' })
+const accounts = JSON.parse(accountData)
+const users = JSON.parse(userData)
+
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Index'
+    title: 'Account Summary',
+    accounts
+  })
+})
+app.get('/savings', (req, res) => {
+  res.render('account', {
+    account: accounts.savings
+  })
+})
+app.get('/checking', (req, res) => {
+  res.render('account', {
+    account: accounts.checking
+  })
+})
+app.get('/credit', (req, res) => {
+  res.render('account', {
+    account: accounts.credit
+  })
+})
+app.get('/profile', (req, res) => {
+  res.render('profile', {
+    user: users[0]
   })
 })
 
